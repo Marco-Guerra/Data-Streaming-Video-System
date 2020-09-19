4f1555de9d832f207a276000448dcf7f8708be72
Marco-Guerra <marcoguerrapedroso@gmail.com>
2020-09-19 17:42:45 -0400

    modificando estrutura do programa

---
+++ structures/user_structure.h
@@ -0,0 +1,30 @@
+#ifndef STRUCTURES
+#define STRUCTURES
+
+#define MAX_HISTORY 100
+
+#include <iostream>
+using namespace std;
+
+// definição da estrutura para armazenar a data de nascimento de um usuário
+struct DateOfBirthStructure{
+    int day;
+    int month;
+    int year;
+};
+
+// definição da estrutura para armazenar o histórico des vídeos vistos por um usuário
+struct HistoryStructure{
+    string videoIdentifications[MAX_HISTORY];
+    int numberOfVideoIdentification;
+};
+
+// definição da estrutura para armazenar todos os datos de um usuário
+struct UserDocumentStructure{
+    string identification;
+    string name;
+    DateOfBirthStructure date;
+    HistoryStructure history;
+};
+
+#endif
---
+++ structures/video_structure.h
@@ -0,0 +1,34 @@
+#ifndef VIDEO_STRUCTURE
+#define VIDEO_STRUCTURE
+
+#define MAX_GENRES 10
+
+#include <iostream>
+using namespace std;
+
+// definição da estrutura para armazenar o tempo de duração de um vídeo
+struct DurationStructure{
+    unsigned int hours;
+    unsigned int minutes;
+    unsigned int seconds;
+};
+
+// definição da estrutura para armazenar os gêneros ao qual um vídeo pertence
+struct GenresStructure{
+    string genres[MAX_GENRES];
+    int numberOfGenre;
+};
+
+// definição da estrutura para armazenar todos os dados de um vídeo
+struct VideoDocumentStructure{
+    string identification;
+    string kindOfVideo;
+    string name;
+    string directorName;
+    DurationStructure durartion;
+    int numberOfSeasons;
+    int releaseYear;
+    GenresStructure genres;
+};
+
+#endif

48da403a37a889ba11ed5a3901b1db29643ab99f
Victor Emanuel Almeida <victoralmeida2001@hotmail.com>
2020-09-29 16:26:21 -0300

    Simplifiqeuei o compile.sh

--- compile.sh
+++ compile.sh
@@ -3,11 +3,13 @@
 
 # colocar como leitura recursiva se possivel
 
-g++ main.cpp \
-    controllers/user_controller/*.cpp \
-    controllers/video_controller/*.cpp \
-    interfaces/user_interface/*.cpp \
-    interfaces/video_interface/*.cpp \
--o main.out
+# g++ main.cpp \
+#     controllers/user_controller/*.cpp \
+#     controllers/video_controller/*.cpp \
+#     interfaces/user_interface/*.cpp \
+#     interfaces/video_interface/*.cpp \
+# -o main.out
+
+g++ main.cpp ./*/*/*.cpp -o main.out
 
 exit 0

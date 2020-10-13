60bb70eb0cb116b68d66a3f6c86f4a57763e342d
Gustavo Borges <71459239+gBorges19@users.noreply.github.com>
2020-10-13 00:11:25 -0300

    Update read_stdio_user.hpp

--- interfaces/user_interface/read_stdio_user.hpp
+++ interfaces/user_interface/read_stdio_user.hpp
@@ -1,40 +1,40 @@
-#ifndef READ_STDIO_USER
-#define READ_STDIO_USER
+    #ifndef READ_STDIO_USER
+    #define READ_STDIO_USER
 
-#include "../../structures/user_structure.hpp"
+    #include "../../structures/user_structure.hpp"
 
-#include <string>
+    #include <string>
 
-using namespace std;
+    using namespace std;
 
-// função que faz a leitura da saída estandar a identificação de um usuário
-string readStdioUserIdentificatio();
+    // função que faz a leitura da saída estandar a identificação de um usuário
+    string readStdioUserIdentificatio();
 
-// função que faz a leitura da saída estandar o nome de um usuário
-string readStdioUserName();
+    // função que faz a leitura da saída estandar o nome de um usuário
+    string readStdioUserName();
 
-// função que faz a leitura da saída estandar o dia da data de aniversario de um usuário
-int readStdioUserDay();
+    // função que faz a leitura da saída estandar o dia da data de aniversario de um usuário
+    int readStdioUserDay();
 
-// função que faz a leitura da saída estandar o mes da data de aniversario de um usuário
-int readStdioUserMonth();
+    // função que faz a leitura da saída estandar o mes da data de aniversario de um usuário
+    int readStdioUserMonth();
 
-// função que faz a leitura da saída estandar o ano da data de aniversario de um usuário
-int readStdioUserYear();
+    // função que faz a leitura da saída estandar o ano da data de aniversario de um usuário
+    int readStdioUserYear();
 
-// função que faz a leitura da saída estandar a data de aniversario de um usuário
-DateOfBirthStructure readStdioUserDateOfBirth();
+    // função que faz a leitura da saída estandar a data de aniversario de um usuário
+    DateOfBirthStructure readStdioUserDateOfBirth();
 
-// função que faz a leitura da saída estandar a identificação de um vídeo visto por um usuário
-string readStdioUserVideosIdentification();
+    // função que faz a leitura da saída estandar a identificação de um vídeo visto por um usuário
+    string readStdioUserVideosIdentification();
 
-// função que faz a leitura da saída estandar a identificação dos vídeos visto por um usuário
-HistoryStructure readStdioUserVideosIdentifications();
+    // função que faz a leitura da saída estandar a identificação dos vídeos visto por um usuário
+    HistoryStructure readStdioUserVideosIdentifications();
 
-// função que faz a leitura da saída estandar um usuário
-UserDocumentStructure readStdioUser();
+    // função que faz a leitura da saída estandar um usuário
+    UserDocumentStructure readStdioUser();
 
-// função que faz a leitura da saída estandar todos os usuário
-UserDocumentStructure readStdioUserList();
+    // função que faz a leitura da saída estandar todos os usuário
+    UserDocumentStructure readStdioUserList();
 
-#endif
+    #endif
